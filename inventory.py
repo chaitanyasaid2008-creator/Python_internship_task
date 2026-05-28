88e63056c0e383251d92b9cb7a80fc0141fa9960
Chaitanya Reddy <chaitanyasaid2008@gmail.com>
2026-05-28 21:30:54 +0530

    Add inventory data structures and print statements

---
+++ inventory.py
@@ -0,0 +1,8 @@
+list=["pen","pencil","eraser","sharpner"]
+tuple=("car","bike","bus","train")
+set={100,200,300,400}
+dictionary={"car":100,"bus":50,"bike":35,"train":15}
+print("list :",list)
+print("tuple :",tuple)
+print("set :",set)
+print("dictionary :",dictionary)
